f16bed6101217111c03956fe16d97f78e8272464
Brian Blais <bblais@gmail.com>
2026-09-09 12:45:26 -0400

    updated install

--- VidPhysics.bat
+++ VidPhysics.bat
@@ -1,2 +1,2 @@
-call .venv\Scripts\activate.bat
-python VidPhysics.py
+uv run VidPhysics.py
+
--- install_python.bat
+++ install_python.bat
@@ -1,4 +1,1 @@
 uv python install 3.11
-uv venv --clear
-call .venv\Scripts\activate.bat
-uv pip install -r requirements.txt
